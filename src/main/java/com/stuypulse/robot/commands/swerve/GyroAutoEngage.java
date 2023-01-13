31f37deb1a5cbdbdb46ad0bf48407c40cca1e018
anivanchen <ivanchen07@gmail.com>
2023-01-13 08:03:58 -0500

    Add auto engage using gyro feedback
    Co-authored-by: Carmin Vuong <carminvuong@users.noreply.github.com>
    Co-authored-by: ambers7 <ambers7@users.noreply.github.com>
    Co-authored-by: Kelvin Zhao <t1rbr@users.noreply.github.com>

---
+++ src/main/java/com/stuypulse/robot/commands/swerve/GyroAutoEngage.java
@@ -0,0 +1,46 @@
+package com.stuypulse.robot.commands.swerve;
+
+import com.stuypulse.robot.subsystems.SwerveDrive;
+import com.stuypulse.stuylib.math.Vector2D;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class GyroAutoEngage extends CommandBase {
+    
+    private final SwerveDrive swerve;
+
+    public GyroAutoEngage() {
+        this.swerve = SwerveDrive.getInstance();
+
+        addRequirements(swerve);
+    }
+
+    @Override
+    public void execute() {
+        double target = Units.inchesToMeters(172.631);
+
+        double translation = swerve.getTranslation().getX() - target;
+        double velocity = translation * (swerve.getGyroPitch() / 15);
+
+        ChassisSpeeds speeds = new ChassisSpeeds(
+            MathUtil.clamp(velocity, -0.5, +0.5),
+            0,
+            0
+        );
+
+        swerve.setStates(speeds);
+    }
+
+    @Override 
+    public boolean isFinished() {
+        return Math.abs(swerve.getGyroPitch()) < 2;
+    }
+
+    @Override 
+    public void end(boolean interrupted) {
+        swerve.setStates(new Vector2D(0, 0), 0.0);
+    }
+}
