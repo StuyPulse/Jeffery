a2d6389101bfd45a70589e4f8f27a6b5299d6466
anivanchen <ivanchen07@gmail.com>
2023-01-25 18:17:49 -0500

    clean file + increase max speed

--- src/main/java/com/stuypulse/robot/commands/swerve/BasicGyroEngage.java
+++ src/main/java/com/stuypulse/robot/commands/swerve/BasicGyroEngage.java
@@ -9,6 +9,7 @@ import com.stuypulse.stuylib.network.SmartString;
 import com.stuypulse.stuylib.streams.IStream;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class BasicGyroEngage extends CommandBase {
@@ -22,19 +23,21 @@ public class BasicGyroEngage extends CommandBase {
     public BasicGyroEngage(SwerveDrive swerve) {
         this.swerve = swerve;
 
-        MAX_TILT = new SmartNumber("Auto Engage/Max Tilt (deg)", 15.0);
-        MAX_ENGAGE_SPEED = new SmartNumber("Auto Engage/Max Engage Speed (m per s)", 0.25);
+        MAX_TILT = new SmartNumber("Auto Engage/Max Tilt", 15.0);
+        MAX_ENGAGE_SPEED = new SmartNumber("Auto Engage/Max Engage Velocity", 0.4);
 
         control = new PIDController(IStream.create(() -> MAX_ENGAGE_SPEED.get() / MAX_TILT.get()).number(), 0, 0);
     }
 
     @Override
     public void execute() {
-        double speed = control.update(
-            0,
-            -1 * Pitch.calculate(swerve.getGyroPitch(), swerve.getGyroRoll(), swerve.getAngle()).getDegrees());
+        double balanceAngle = -Pitch.calculate(swerve.getGyroPitch(), swerve.getGyroRoll(), swerve.getAngle()).getDegrees();
+        double speed = control.update(0, balanceAngle);
         
         swerve.setStates(new ChassisSpeeds(speed, 0, 0), true);
+
+        SmartDashboard.putNumber("Auto Engage/Balance Angle", balanceAngle);
+        SmartDashboard.putNumber("Auto Engage/Velocity", speed);
     }
 
     @Override
