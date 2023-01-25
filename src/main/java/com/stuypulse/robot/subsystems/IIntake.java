36daff33d8d84af31256705e4d1d017d5a96be3b
Myles Pasetsky <mp3blue99@gmail.com>
2023-01-25 16:35:43 -0500

    Fix stupid stuff

--- src/main/java/com/stuypulse/robot/subsystems/IIntake.java
+++ src/main/java/com/stuypulse/robot/subsystems/IIntake.java
@@ -2,10 +2,4 @@ package com.stuypulse.robot.subsystems;
 
 public abstract class IIntake {
     
-
-    public abstract void intake();
-    public abstract void outtake();
-
-    public abstract void setMode(IntakeMode mode);
-
 }
