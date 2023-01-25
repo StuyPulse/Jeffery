29bb98bae5eef7f0a099e0075b66e1627f36c7f7
anivanchen <ivanchen07@gmail.com>
2023-01-25 18:12:45 -0500

    Make everything not smartnumbers

--- src/main/java/com/stuypulse/robot/commands/swerve/SamAutoEngage.java
+++ src/main/java/com/stuypulse/robot/commands/swerve/SamAutoEngage.java
@@ -13,6 +13,7 @@ import com.stuypulse.stuylib.network.SmartString;
 import com.stuypulse.stuylib.streams.IStream;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class SamAutoEngage extends CommandBase {
@@ -20,41 +21,36 @@ public class SamAutoEngage extends CommandBase {
     private final SwerveDrive swerve;
 
     private final Controller tiltController, velocityController;
-
-    private final SmartAngle BALANCE_ANGLE;
-    private final SmartNumber OFFSET, TARGET, CURRENT_POS, VELOCITY;
     
     public SamAutoEngage(SwerveDrive swerve) {
         this.swerve = swerve;
 
-        tiltController = new PIDController(1,0,0);
+        tiltController = new PIDController(6.0/15.0, 0,0);
         velocityController = new PIDController(1,0,0);
-
-        BALANCE_ANGLE = new SmartAngle("Auto Engage/Balance Angle", Angle.kZero);
-
-        OFFSET = new SmartNumber("Auto Engage/Offset", 0);
-        TARGET = new SmartNumber("Auto Engage/Target", Field.CHARGING_STATION_CENTER.getX());
-        CURRENT_POS = new SmartNumber("Auto Engage/Distance", swerve.getTranslation().getX());
-        VELOCITY = new SmartNumber("Auto Engage/Velocity", 0);
 
         addRequirements(swerve);
     }
 
     @Override
     public void execute() {
-        BALANCE_ANGLE.set(Angle.fromDegrees(Pitch.calculate(swerve.getGyroPitch(), swerve.getGyroRoll(), swerve.getAngle()).getDegrees()));
-        OFFSET.set(tiltController.update(0, BALANCE_ANGLE.get().getRotation2d().getDegrees()));
+        var target = Field.CHARGING_STATION_CENTER.getX();
+        var balanceAngle = Pitch.calculate(swerve.getGyroPitch(), swerve.getGyroRoll(), swerve.getAngle()).getDegrees();
+        var offset = tiltController.update(0, balanceAngle);
 
-        TARGET.add(OFFSET);
-        CURRENT_POS.set(swerve.getTranslation().getX());
-        VELOCITY.set(velocityController.update(TARGET.get(), CURRENT_POS.get()));
+        target += offset;
+        var position = swerve.getTranslation().getX();
+        var velocity = velocityController.update(target, position);
 
-        swerve.setStates(new ChassisSpeeds(VELOCITY.get(), 0.0, 0.0), true);
+        swerve.setStates(new ChassisSpeeds(velocity, 0.0, 0.0), true);
+        
+        SmartDashboard.putNumber("Auto Engage/position", position);
+        SmartDashboard.putNumber("Auto Engage/vel", velocity);
     }
 
     @Override 
     public boolean isFinished() {
-        return velocityController.isDone(0.1);
+        // return velocityController.isDone(0.1);
+        return false;
     }
 
     @Override 
